63813e458d7d6a7cc3525fddbfe38ab63d18491a
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2020-06-26 22:47:03 +0200

    fourth week's solution

---
+++ week-04/patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,57 @@
+package patmat
+
+import org.junit._
+import org.junit.Assert.assertEquals
+
+class HuffmanSuite {
+  import Huffman._
+
+  trait TestTrees {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+  }
+
+
+  @Test def `weight of a larger tree (10pts)`: Unit =
+    new TestTrees {
+      assertEquals(5, weight(t1))
+    }
+
+
+  @Test def `chars of a larger tree (10pts)`: Unit =
+    new TestTrees {
+      assertEquals(List('a','b','d'), chars(t2))
+    }
+
+  @Test def `string2chars hello world`: Unit =
+    assertEquals(List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'), string2Chars("hello, world"))
+
+
+  @Test def `make ordered leaf list for some frequency table (15pts)`: Unit =
+    assertEquals(List(Leaf('e',1), Leaf('t',2), Leaf('x',3)), makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))))
+
+
+  @Test def `combine of some leaf list (15pts)`: Unit = {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assertEquals(List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)), combine(leaflist))
+  }
+
+
+  @Test def `decode and encode a very short text should be identity (10pts)`: Unit =
+    new TestTrees {
+      assertEquals("ab".toList, decode(t1, encode(t1)("ab".toList)))
+    }
+
+  @Test def `times`: Unit =
+    new TestTrees {
+      assertEquals(Huffman.times(List('a', 'b', 'a')), List(('a', 2), ('b', 1)))
+    }
+
+  @Test def `french`: Unit =
+    assertEquals(encode(frenchCode)(List('e', 'n', 'c', 'o', 'r', 'e', 'u', 'n', 't', 'e', 'x', 't', 'e', 't', 'r', 'e', 's', 's', 'e', 'c', 'r', 'e', 't')), List(1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1))
+
+  @Test def `createCodeTree`: Unit =
+    assertEquals(Huffman.createCodeTree("someText".toList), Leaf('d', 1))
+
+  @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
+}
